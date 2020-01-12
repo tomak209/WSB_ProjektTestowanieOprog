279d8621aca91b4ddef0bd8bbd82ad83405b9510
Tomasz <44137002+tomak209@users.noreply.github.com>
2020-01-12 14:30:18 +0100

    Update save_box_page.py

--- page_objects/save_box_page.py
+++ page_objects/save_box_page.py
@@ -1,9 +1,4 @@
-import time
 from config import strings
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 from page_objects.base_page import BasePage
 
 class SaveBox(BasePage):
@@ -12,3 +7,33 @@ class SaveBox(BasePage):
         self.browser = browser
         self.browser.get(strings.save_box_url)
         self.browser.implicitly_wait(strings.timeout)
+
+    def page_opened(self):
+        text = self.browser.find_element_by_xpath('//*[@id="js-mainWrapper"]/main/div[7]/div/ul/li[7]/span[2]').text
+        if text == 'Odkurzacz workowy ELECTROLUX EPF61RR':
+            return True
+        else:
+            return False
+
+    @property
+    def cubby(self):
+        return self.browser.find_elements_by_class('icons-schowek01')
+    @property
+    def cross(self):
+        return self.browser.find_elements_by_class('mfp-close')
+
+    def add_cubby(self):
+        self.browser.execute_script("arguments[0].click();", self.cubby)
+        info = self.browser.find_elements_by_class('m-typo m-typo_secondary').txt
+        if info == "Produkt został dodany do schowka":
+            return self.browser.execute_script("arguments[0].click();", self.cross) is True
+        else:
+            return False
+
+    def sub_cubby(self):
+        self.browser.execute_script("arguments[0].click();", self.cubby)
+        info = self.browser.find_elements_by_class('m-typo m-typo_secondary').txt
+        if info == "Produkt został usunięty ze schowka":
+            return self.browser.execute_script("arguments[0].click();", self.cross) is True
+        else:
+            return False
