671aa846bd0fe066173c0bdc210047e77e5efcce
Tomasz <44137002+tomak209@users.noreply.github.com>
2020-01-12 13:38:04 +0100

    Update search_page.py
    aktualizacja

--- page_objects/search_page.py
+++ page_objects/search_page.py
@@ -14,26 +14,49 @@ class SearchPage(BasePage):
         self.browser.implicitly_wait(strings.timeout)
 
     @property
-    def input_inner(self):
-        return self.browser.find_element_by_id("query_querystring")
+    def input_search(self):
+        return self.browser.find_element_by_class('m-search_inputInner is-active')
 
     @property
     def icon_search(self):
-        return self.browser.find_element_by_class("beheader-search")
+        return self.browser.find_element_by_id('beheader-search')
 
     def correct_search(self):
-        self.browser.input_inner.send_key(strings.search_data("1"))
+        self.input_search.send_key(strings.search_data['a'])
         self.browser.execute_script("arguments[0].click();", self.icon_search)
+        self.browser.implicitly_wait(strings.timeout)
+        category = self.browser.find_element_by_xpath('//*[@id="js-mainWrapper"]/main/div[4]/h1')
+        if category.txt == "Monitory ":
+            return True
+        else:
+            return False
 
     def incorrect_search_1(self):
-        self.browser.input_inner.send_key(strings.search_data("2"))
+        self.browser.input_inner.send_key(strings.search_data("b"))
         self.browser.execute_script("arguments[0].click();", self.icon_search)
+        article_title = self.browser.find_element_by_xpath('//*[@id="js-mainWrapper"]/main/div[4]/section/article/header/h1/text()[2]')
+        if article_title == "Niestety nie udało się znaleźć wyszukiwanego produktu.":
+            return True
+        else:
+            return False
 
     def incorrect_search_2(self):
-        self.browser.input_inner.send_key(strings.search_data("3"))
+        self.browser.input_inner.send_key(strings.search_data("c"))
         self.browser.execute_script("arguments[0].click();", self.icon_search)
+        article_title = self.browser.find_element_by_xpath(
+            '//*[@id="js-mainWrapper"]/main/div[4]/section/article/header/h1/text()[2]')
+        if article_title == "Niestety nie udało się znaleźć wyszukiwanego produktu.":
+            return True
+        else:
+            return False
 
     def incorrect_search_3(self):
-        self.browser.input_inner.send_key(strings.search_data("4"))
+        self.browser.input_inner.send_key(strings.search_data("d"))
         self.browser.execute_script("arguments[0].click();", self.icon_search)
+        article_title = self.browser.find_element_by_xpath(
+            '//*[@id="js-mainWrapper"]/main/div[4]/section/article/header/h1/text()[2]')
+        if article_title == "Niestety nie udało się znaleźć wyszukiwanego produktu.":
+            return True
+        else:
+            return False
 
